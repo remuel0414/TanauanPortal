1a38c7665ac7f08c4e94676c723f3ef569a34f8b
remuel144 <146079776+remuel144@users.noreply.github.com>
2024-04-16 17:11:45 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/TanauanPortal/', // Set the base URL to the relative path of your GitHub Pages URL
+  base: '/https://github.com/remuel0414/TanauanPortal.git/', // Set the base URL to the relative path of your GitHub Pages URL
   plugins: [react()],
   server: {
     port: 3000,
